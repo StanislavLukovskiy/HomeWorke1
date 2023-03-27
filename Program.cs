9e34b88a743385714b225dcc6e2278d78dc1d463
stas <point.blank.point@bk.ru>
2023-03-28 00:26:20 +0300

    проблема с макс

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число");
+string value;
+value = Console.ReadLine();
+int numberA = Convert.ToInt32(value);
+
+Console.Write("Введите второе число");
+value = Console.ReadLine();
+int numberB = Convert.ToInt32(value);
+
+Console.Write("Введите третье число");
+value = Console.ReadLine();
+int numberD = Convert.ToInt32(value);
+int max = numberA;
+if ((numberA > numberB) && (numberA > numberD))
+{
+   int max = numberA;
+}
+else
+if ((numberB > numberA) && (numberB > numberD))
+{    
+  int max = numberB;
+}
+else
+if ((numberD > numberA) && (numberD > numberB))
+{   
+  int max = numberD;
+}
+Console.WriteLine("Найбольшее из трёх ");
+Console.Write(max);
